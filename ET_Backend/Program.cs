7cc155e88603b034ed15f063fe5c75992d12ab62
Krishanshu-Kumar <krish266.kk@gmail.com>
2025-06-03 19:00:08 +0530

    Initial Commit Setup

---
+++ ET_Backend/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Enable Swagger middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/hello", () => "Hello World!")
+   .WithOpenApi(); // Optional: makes endpoint show up in Swagger UI
+
+app.Run();
